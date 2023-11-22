1ad4fb13abe6d779cea005a6d6f8b3e269d352d2
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-11-22 13:07:35 +0100

    refactor tag styles

---
+++ src/config/tags-style.ts
@@ -0,0 +1,7 @@
+export const tag_styles = {
+    algorithms: 'bg-red-400/50 dark:text-red-100 text-red-900',
+    engineering: 'bg-green-400/50 dark:text-green-100 text-green-900',
+    haskell: 'bg-purple-400/50 dark:text-purple-100 text-purple-900',
+    javascript: 'bg-yellow-400/50 dark:text-yellow-100 text-yellow-900',
+    typescript: 'bg-blue-400/50 dark:text-blue-100 text-blue-900',
+} as const
