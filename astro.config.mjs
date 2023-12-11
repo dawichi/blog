c65db729fdc1904007403b2bf6c65978a4cb00f3
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-11 18:55:00 +0100

    Revert "add vercel analytics"
    This reverts commit c5616486f2b9d6a27479190890b53fc2b2bd1cd9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [mdx(), tailwind()],
-    output: 'server',
-    adapter: vercel(),
 })
